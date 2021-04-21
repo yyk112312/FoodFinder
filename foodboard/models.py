a4f64d30e7e87bf55c7253fe5ce70041096aa75f
YYK <dbdudrb21@gmail.com>
2021-04-21 17:31:51 +0900

    자잘한 수정 foodboard부분 및 여러가지

--- foodboard/models.py
+++ foodboard/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Foodclass(models.Model):
     name = models.CharField('식품명', max_length=50, unique=False)
     once = models.FloatField('1회 제공량', default=0)
+    calories = models.FloatField('칼로리', default=0)
     cabor = models.FloatField('탄수화물', default=0)
     protein = models.FloatField('단백질', default=0)
     fat = models.FloatField('지방', default=0)
